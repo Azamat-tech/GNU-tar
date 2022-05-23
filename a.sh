3d6f2c1934e0ac722df0f0a3e71b3a85ca95d6a3
Azamat Zarlykov <azarlykovcz@gmail.com>
2022-05-24 01:04:55 +0200

    update

--- a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export GNUTAR="$(which tar)"
-export MYTAR_C="$(readlink -e mytar.c)"
-export STEF="$(readlink -e stef/stef.sh)"
-chmod -R +x tests/
-cd tests/
-./run-tests.sh $(cat phase-1.tests)
-rm -rf dir.*
